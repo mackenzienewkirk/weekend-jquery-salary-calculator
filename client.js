db1202a8f75e867ab32ca7de04ca7b3a1734eae5
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-03 11:47:23 -0600

    finished creating addEmployeeInfo function

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ $(document).ready(readyNow);
 let employeeTable = [];
 
 function readyNow() {
+    render()
     console.log("DOM is loaded!");
     // add click event handler that calls the addEmployeeInfo function.
     $('').on('click', addEmployeeInfo);
@@ -16,13 +17,26 @@ function addEmployeeInfo() {
     let addedJobTitle = $('#jobTitleInput').val()
     let addedAnnualSalary = $('#annualSalaryInput').val()
 
+//Create an object for each employee to be pushed into the emplyeeTable array.
 
     let employee = {
-    firstName: addedFirstName,
-    lastName: addedLastName,
-    iDNumber: Number(addedIDNumber),
-    jobTitle: addedJobTitle,
-    annualSalary: Number(addedAnnualSalary)
+        firstName: addedFirstName,
+        lastName: addedLastName,
+        iDNumber: Number(addedIDNumber),
+        jobTitle: addedJobTitle,
+        annualSalary: Number(addedAnnualSalary)
 
     }
+    employeeTable.push(employee);
+    render();
+
+    //Call jQuery to get the value of each input
+
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#iDNumberInput').val('');
+    $('#jobTitleInput').val('');
+    $('#annualSalaryInput').val('');
+    
 }
+
