56a05ff70cebb87589b7cb2989b0fa992221f06d
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-05 11:27:42 -0600

    updates

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ $(document).ready(onReady);
 
 
 let employees = [];
+
 let monthlyCost = 0;
 let monthlyCostMax = 20000;
 
@@ -10,10 +11,32 @@ function onReady() {
     // add click event handler that calls the addEmployeeInfo function.
     $('#submitButton').on('click', addEmployeeInfo);
     // add click event handler that calls the deleteEmployeeInfo function.
-    $('body').on('click','.deleteEmployeeInfo', deleteEmployeeInfo)
+    $('body').on('click', deleteEmployeeInfo)
+    renderEmployeeTable();
 }
 
-//A function to add the employees into the employeeTable array//
+function renderEmployeeTable() {
+    //Clear out current content on the table.
+    //Loop through the employees array
+    //Append a <tr></tr> containing employee data to the DOM
+    console.log('called renderEmployeeTable');
+    $('#employeeRows').empty();
+    for (let employee of employees) {
+        $('#employeeRows').append(`
+        <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName} </td>
+            <td>${employee.jobTitle} </td>
+            <td>${employee.iDNumber} </td>
+            <td>${employee.annualSalary}</td>
+            <button id="submitButton">Submit</button>
+            <td><button class="delete">Delete!</button></td>
+        </tr>
+            `)
+    }
+}
+
+
 
 function addEmployeeInfo() {
 
@@ -39,41 +62,34 @@ function addEmployeeInfo() {
 
     //Call jQuery to get the value of each input
 
-    render()
+    renderEmployeeTable()
 }
 
 
 
-function addEmployeetoTable() {
-    addEmployeeInfo();
+// function render() {
+//     console.log('this is the render function');
+//     totalMonthlyCost = 0;
+//     $('.employee').empty();
+//     for (let i = 0; i < employees.length; i++) {
+//         $('.employee').append(`
+//         <tr>
+//             <td> ${employees[i].firstName}</td>
+//             <td>${employees[i].lastName} </td>
+//             <td>${employees[i].jobTitle} </td>
+//             <td>${employees[i].iDNumber} </td>
+//             <td>${employees[i].annualSalary}</td>
+//             <button id="submitButton">Submit</button>
+//             <td><button class="delete">Delete!</button></td>
+//         </tr>
+//     `)
+//     monthlyCost = employees[i].annualSalary / 12;
+//     totalMonthlyCost += monthlyCost;
     
-    render()
-}
-
-
-function render() {
-    console.log('this is the render function');
-    totalMonthlyCost = 0;
-    $('.employee').empty();
-    for (let i = 0; i < employees.length; i++) {
-        $('.employee').append(`
-        <tr>
-            <td> ${employees[i].firstName}</td>
-            <td>${employees[i].lastName} </td>
-            <td>${employees[i].jobTitle} </td>
-            <td>${employees[i].iDNumber} </td>
-            <td>${employees[i].annualSalary}</td>
-            <button id="submitButton">Submit</button>
-            <td><button class="delete">Delete!</button></td>
-        </tr>
-    `)
-    monthlyCost = employees[i].annualSalary / 12;
-    totalMonthlyCost += monthlyCost;
-    
-    deleteEmployeeInfo();
-    monthlyCostMaxFontColor();
-}
-}
+//     deleteEmployeeInfo();
+//     monthlyCostMaxFontColor();
+// }
+// }
 
 
 function deleteEmployeeInfo() {
